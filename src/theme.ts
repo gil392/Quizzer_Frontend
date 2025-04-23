dccfe9e63c83bd9898205fe6dacb7eb3b8a336ae
Tomer Shomron <tomercpc01@gmail.com>
2025-04-24 02:07:31 +0300

    added theme

---
+++ src/theme.ts
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#6750A4',
+            contrastText: '#FFFFFF'
+        },
+        secondary: {
+            main: '#625B71'
+        },
+        background: {
+            default: '#FFFBFE',
+            paper: '#FFFFFF'
+        },
+        error: {
+            main: '#B3261E'
+        },
+        text: {
+            primary: '#1C1B1F',
+            secondary: '#49454F'
+        }
+    }
+});
+
+export default theme;
