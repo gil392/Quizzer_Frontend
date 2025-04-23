fe8f4e3e084226aa94947b5cebe0bc299533976d
Tomer Shomron <tomercpc01@gmail.com>
2025-04-24 02:10:01 +0300

    use theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import './App.css'
-import SummaryPage from './pages/Summary'
+import { ThemeProvider } from '@mui/material';
+import {
+    Navigate,
+    Route,
+    BrowserRouter as Router,
+    Routes
+} from 'react-router-dom';
+import './App.css';
 import QuizPage from './pages/Quiz';
-import HomePage from './pages/home/Home'
-
+import SummaryPage from './pages/Summary';
+import HomePage from './pages/home/Home';
+import RegisterPage from './pages/register/RegisterPage';
+import quizzerTheme from './theme';
 
 function App() {
-
   return (
-    <Router>
-            <Routes>
-                <Route path="/" element={<Navigate to="/home" replace />} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/summary" element={<SummaryPage />} />
-                <Route path="/quiz" element={<QuizPage />}
-                />
-            </Routes>
-    </Router>
-    
-  )
+    <ThemeProvider theme={quizzerTheme}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navigate to='/home' replace />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='/summary' element={<SummaryPage />} />
+          <Route path='/quiz' element={<QuizPage />} />
+          <Route path='/signup' element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
