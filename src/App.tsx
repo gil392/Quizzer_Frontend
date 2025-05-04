e6f4567e2c47294c6ae9cf90a5070c197922339d
Tomer Shomron <tomercpc01@gmail.com>
2025-05-04 23:39:05 +0300

    use appbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
-import { ThemeProvider } from '@mui/material';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
-import Layout from './components/layout/Layout';
-import quizzerTheme from './theme';
-import NavBar from './components/navBar/NavBar';
+import { ThemeProvider } from "@mui/styles";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import AppBar from "./components/appbar/Appbar";
+import Layout from "./components/layout/Layout";
+import NavBar from "./components/navBar/NavBar";
+import quizzerTheme from "./theme";
 
 function App() {
-    return (
-        <ThemeProvider theme={quizzerTheme}>
-            <div
-                style={
-                    {
-                        width: '100%',
-                        height: '100%',
-                        '--primary-main': quizzerTheme.palette.primary.main,
-                        '--primary-light': quizzerTheme.palette.primary.light,
-                        '--primary-contrastText':
-                            quizzerTheme.palette.primary.contrastText
-                    } as React.CSSProperties
-                }
-            >
-                <Router>
-                    <NavBar />
-                    <Layout />
-                </Router>
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={quizzerTheme}>
+      <div
+        style={
+          {
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            "--primary-main": quizzerTheme.palette.primary.main,
+            "--primary-light": quizzerTheme.palette.primary.light,
+            "--primary-contrastText": quizzerTheme.palette.primary.contrastText,
+          } as React.CSSProperties
+        }
+      >
+        <Router>
+          <NavBar />
+          <div
+            style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}
+          >
+            <AppBar />
+            <Layout />
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
