bf1f2e4e0313a9193f5353b0da2b60248864e171
Tomer Shomron <tomercpc01@gmail.com>
2025-05-05 22:11:21 +0300

    created friend item

---
+++ src/pages/friends/components/FriendItem/FriendItem.tsx
@@ -0,0 +1,37 @@
+import { Avatar, Typography } from "@mui/material";
+import clsx from "clsx";
+import { FunctionComponent } from "react";
+import { User } from "../../../../api/user/types";
+import { useStyles } from "./styles";
+
+interface FriendItemProps {
+  user: User;
+  className?: string;
+}
+
+const FriendItem: FunctionComponent<FriendItemProps> = (props) => {
+  const { user, className } = props;
+  const classes = useStyles();
+
+  return (
+    <div className={clsx(className, classes.root)}>
+      <div className={classes.avatarBox}>
+        <Avatar
+          src={user.picture ?? "error"}
+          alt={user.username.toUpperCase()}
+        />
+      </div>
+      <div>
+        <Typography>{user.username}</Typography>
+        <Typography className={classes.statisticText} color="textSecondary">
+          Streak: {user.statistics.streak}d
+        </Typography>
+        <Typography className={classes.statisticText} color="textSecondary">
+          XP: {user.statistics.xp}
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default FriendItem;
