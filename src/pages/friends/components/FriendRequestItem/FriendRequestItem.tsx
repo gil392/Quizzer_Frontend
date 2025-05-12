df674ed36ea3ae04c4e8803d6cdb22090dba9d0c
Tomer Shomron <tomercpc01@gmail.com>
2025-05-12 23:13:14 +0300

    created FriendRequestItem

---
+++ src/pages/friends/components/FriendRequestItem/FriendRequestItem.tsx
@@ -0,0 +1,51 @@
+import { CheckCircleOutline, HighlightOff } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import clsx from "clsx";
+import { FunctionComponent } from "react";
+import {
+  acceptFriendRequest,
+  declineFriendRequest,
+} from "../../../../api/user/api";
+import { UserWithId } from "../../../../api/user/types";
+import FriendItem from "../FriendItem/FriendItem";
+import { useStyles } from "./styles";
+
+interface FriendRequestItemProps {
+  user: UserWithId;
+  className?: string;
+}
+
+const FriendRequestItem: FunctionComponent<FriendRequestItemProps> = (
+  props
+) => {
+  const { user, className } = props;
+  const classes = useStyles();
+
+  const onAcceptClick = () => {
+    acceptFriendRequest(user._id);
+  };
+
+  const onDeclineClick = () => {
+    declineFriendRequest(user._id);
+  };
+
+  return (
+    <div className={clsx(className, classes.root)}>
+      <FriendItem user={user} />
+
+      <div className={classes.actions}>
+        <IconButton size="small" onClick={onAcceptClick}>
+          <CheckCircleOutline
+            className={classes.acceptButton}
+            fontSize="inherit"
+          />
+        </IconButton>
+        <IconButton size="small" onClick={onDeclineClick}>
+          <HighlightOff className={classes.declienButton} fontSize="inherit" />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default FriendRequestItem;
