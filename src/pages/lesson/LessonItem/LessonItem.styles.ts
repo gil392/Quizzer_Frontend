0eb8ef5824ae1781a0c58554a069b70b2f85e03f
Omer Hasid <omerhasid11@gmail.com>
2025-05-16 20:19:45 +0300

    set lesson item dark mode

--- src/pages/lesson/LessonItem/LessonItem.styles.ts
+++ src/pages/lesson/LessonItem/LessonItem.styles.ts
@@ -1,5 +1,5 @@
 import { makeStyles, createStyles } from "@mui/styles";
-import { Theme } from "@mui/material/styles";
+import { Theme } from "@mui/material";
 
 const useStyles = makeStyles((theme: Theme) => {
   const isDarkMode = theme.palette.mode === "dark";
