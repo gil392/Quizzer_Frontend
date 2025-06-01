055139f4d640a24ae48b3281fef0ea001b43b64d
Tomer Shomron <tomercpc01@gmail.com>
2025-06-01 04:27:12 +0300

    reuse scroller styles

--- src/pages/profile/components/Achievments/styles.ts
+++ src/pages/profile/components/Achievments/styles.ts
@@ -1,5 +1,6 @@
 import { Theme } from "@mui/material";
 import { createStyles, makeStyles } from "@mui/styles";
+import { createScrollerStyleClass } from "../../../../styles/quizzer.styles";
 
 export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -17,26 +18,7 @@ export const useStyles = makeStyles((theme: Theme) =>
       borderColor: theme.palette.grey[200],
       borderRadius: "8px",
     },
-    scroller: {
-      "&::-webkit-scrollbar": {
-        width: "8px",
-      },
-      "&::-webkit-scrollbar-thumb": {
-        backgroundColor:
-          theme.palette.mode === "light"
-            ? "rgba(0, 0, 0, 0.2)"
-            : "rgba(255, 255, 255, 0.8)",
-        borderRadius: "4px",
-        border: "2px solid transparent",
-        backgroundClip: "content-box",
-      },
-      "&::-webkit-scrollbar-thumb:hover": {
-        backgroundColor:
-          theme.palette.mode === "light"
-            ? "rgba(0, 0, 0, 0.4)"
-            : "rgba(255, 255, 255, 0.6)",
-      },
-    },
+    scroller: createScrollerStyleClass(theme),
     skeletonItem: {
       "&.MuiSkeleton-root": {
         height: "4em",
