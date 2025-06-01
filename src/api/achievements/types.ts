7890924e08e2713e4597153dd37bf6706af76b9b
Tomer Shomron <tomercpc01@gmail.com>
2025-06-01 03:01:16 +0300

    add api for achievements

---
+++ src/api/achievements/types.ts
@@ -0,0 +1,13 @@
+export type Reward = {
+  icon: string;
+  xp: number;
+};
+
+export type RequirmentProgress = { count: number; value: number };
+
+export type Achievement = {
+  title: string;
+  description: string;
+  reward: Reward;
+  requirements: RequirmentProgress[];
+};
