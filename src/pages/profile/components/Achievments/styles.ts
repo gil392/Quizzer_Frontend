99ca2be0df5b6e28fa5a97ca490f0716a714ef65
Tomer Shomron <tomercpc01@gmail.com>
2025-06-02 08:12:59 +0300

    fix style

--- src/pages/profile/components/Achievments/styles.ts
+++ src/pages/profile/components/Achievments/styles.ts
@@ -6,7 +6,7 @@ export const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: "33%",
-      flexGrow: 1,
+      height: "65%",
       display: "flex",
       flexDirection: "column",
     },
