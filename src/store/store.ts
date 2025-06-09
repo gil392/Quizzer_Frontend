9284780f7be46e0a8609e42c0e503b49a5d613f9
Gil <gilgil.abramovich@gmail.com>
2025-06-09 18:07:06 +0300

    When logout is fulfilled, clear all the store state. That's to prevent current bug of attempts staying between different users. This is also natural- we "start over".
    Make logout work with dispatch (moved createAppbarMenu from utils to use dispatch more freely).

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, AnyAction } from "@reduxjs/toolkit";
 import lessonReducer from "./lessonReducer";
 import quizReducer from "./quizReducer";
-import userReducer from "./userReducer";
+import userReducer, { logoutAsync } from "./userReducer";
 import attemptReducer from "./attemptReducer";
 
+const appReducer = combineReducers({
+  lessons: lessonReducer,
+  quizzes: quizReducer,
+  user: userReducer,
+  attempt: attemptReducer,
+});
+
+const rootReducer = (
+  state: ReturnType<typeof appReducer> | undefined,
+  action: AnyAction
+) => {
+  if (logoutAsync.fulfilled.match(action)) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
 export const store = configureStore({
-  reducer: {
-    lessons: lessonReducer,
-    quizzes: quizReducer,
-    user: userReducer,
-    attempt: attemptReducer,
-  },
+  reducer: rootReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
