ddf0f21475ed0e44d2ad7fd4c6ddca4d2882cd07
Omer Hasid <omerhasid11@gmail.com>
2025-06-12 18:30:51 +0300

    create friends not found component

---
+++ src/pages/friends/components/FriendsPannel/NoFriendsFound/NoFriendsFound.tsx
@@ -0,0 +1,24 @@
+import { Box, Paper, Stack, Typography } from "@mui/material";
+import React from "react";
+import { useStyles } from "./styles";
+
+const NoFriendsFound: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={3} className={classes.root}>
+      <Stack alignItems="center" p={4}>
+        <Box
+          component="img"
+          src="https://img.freepik.com/premium-vector/two-little-kids-study-together-reading-book-illustration_7084-612.jpg"
+          className={classes.container}
+        />
+        <Typography variant="h4">
+          Learn And Share Lessons With Friends
+        </Typography>
+      </Stack>
+    </Paper>
+  );
+};
+
+export default NoFriendsFound;
---
+++ src/pages/friends/components/FriendsPannel/NoFriendsFound/styles.ts
@@ -0,0 +1,17 @@
+import { createStyles, makeStyles } from "@mui/styles";
+
+export const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      borderRadius: 4,
+      textAlign: "center",
+      boxShadow: "0px 8px 30px",
+      backgroundColor: "#fffdf7",
+    },
+    container: {
+      width: "100%",
+      height: "auto",
+      borderRadius: 2,
+    },
+  })
+);
