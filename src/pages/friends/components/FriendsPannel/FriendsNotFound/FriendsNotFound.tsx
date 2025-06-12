c067f94d39443052074fd941096732748821b8e8
Omer Hasid <omerhasid11@gmail.com>
2025-06-12 21:19:45 +0300

    remove paper

--- src/pages/friends/components/FriendsPannel/FriendsNotFound/FriendsNotFound.tsx
+++ src/pages/friends/components/FriendsPannel/FriendsNotFound/FriendsNotFound.tsx
@@ -6,18 +6,14 @@ const FriendsNotFound: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <Paper elevation={3} className={classes.root}>
-      <Stack alignItems="center" p={4}>
-        <Box
-          component="img"
-          src="https://img.freepik.com/premium-vector/two-little-kids-study-together-reading-book-illustration_7084-612.jpg"
-          className={classes.container}
-        />
-        <Typography variant="h4">
-          Learn And Share Lessons With Friends
-        </Typography>
-      </Stack>
-    </Paper>
+    <Stack alignItems="center" p={4}>
+      <Box
+        component="img"
+        src="/images/friendsNotFound.png"
+        className={classes.container}
+      />
+      <Typography variant="h4">Learn And Share Lessons With Friends</Typography>
+    </Stack>
   );
 };
 
