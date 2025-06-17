5c6264ac0f969cfc113828e7399fb74547d69f08
Gil <gilgil.abramovich@gmail.com>
2025-06-17 14:43:16 +0300

    Fixed bug that in a specific situation LessonInfo had overflow-x for some reason. Some gap between lesson image and text.

--- src/pages/lesson/LessonInfo/LessonInfo.styles.ts
+++ src/pages/lesson/LessonInfo/LessonInfo.styles.ts
@@ -6,6 +6,7 @@ const useStyles = makeStyles(() =>
       display: "flex",
       gap: "2vw",
       maxWidth: "100%",
+      minWidth: 0, // allows box to shrink properly, otherwise there's an overflow
       overflowY: "hidden",
     },
     leftBox: {
